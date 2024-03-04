0482326bcf9542e1a23b24370387026ba01ea08f
jpdieter <justindieter@gmail.com>
2024-03-04 14:29:42 -0800

    removed console.log for req.user

--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@ const Review = require('./models/review')
 //created for middleware and added const to campgrounds.js > router.get isLoggedIn
 module.exports.isLoggedIn = (req, res, next) => {
     //passport allows req.user to track current user
-    console.log('req.user...', req.user);
+    // console.log('req.user...', req.user);
     if(!req.isAuthenticated()){
         req.session.returnTo = req.originalUrl; //return to original url
         req.flash('error', 'you must be signed in')
