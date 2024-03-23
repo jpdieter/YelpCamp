2c258e3ea81a4828eb5504289d19b68efacfaab1
jpdieter <justindieter@gmail.com>
2024-03-23 15:40:56 -0700

    removed const secret value

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV !== "production") {
         app.set('trust proxy', 1) // trust first proxy
       }
     
-    const secret = process.env.SECRET || 'ToLiveIsToSuffer1985!@#';
+    const secret = process.env.SECRET 
     
     const store = new MongoStore({
         url: dbURL, 
